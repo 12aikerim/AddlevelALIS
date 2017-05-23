85a74037d5fd4a29b314ece487198760c4208d8c
admalisbend <admalisbend>
2017-05-23 18:18:44 +0000

    profile complete

--- js/profile.js
+++ js/profile.js
@@ -87,7 +87,113 @@
                 alert(msg);
             }
         });
-    })
+    });
 
+    $('#profile-edit-btn').on('click', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: siteRoot + '/constant/positions',
+            type: 'get',
+            success: function (data, textStatus, jqxhr) {
+                $('#profile-position').find('select').first().append(create_options(data));
+            }
+        });
+        var selects = $('#profile-edit-form').find('select');
+        var inputs = $('#profile-edit-form').find('input');
+        inputs.removeAttr('disabled');
+        inputs.removeClass('no_field');
+        selects.removeAttr('disabled');
+        selects.removeClass('no_field');
+        $('#profile-edit-form').find('input[name=email]').attr('disabled', 'disabled');
+        $('#profile-edit-form').find('input[name=email]').addClass('no_field');
+        $(this).attr('hidden', 'hidden');
+        $('#profile-edit-form').find('input[type=submit]').removeAttr('hidden');
+
+    });
+
+    $('#profile-edit-form').on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: siteRoot + $(this).attr('action'),
+            type: 'put',
+            data: $(this).serializeArray(),
+            success: function (data, textStatus, jqxhr) {
+                document.location.reload();
+            },
+            error: function (jqxhr, textStatus, errorThrown) {
+                // todo: display errors
+                var errors = jqxhr.responseJSON;
+                msg = "Errors:\n";
+                for (var key in errors) {
+                    msg += key + ": " + errors[key][0] + '\n';
+                }
+                alert(msg);
+            }
+        });
+    });
+
+    $('#company-edit-btn').on('click', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: siteRoot + '/constant/countries',
+            type: 'get',
+            success: function (data, textStatus, jqxhr) {
+                $('#company-country').find('select').first().append(create_options(data));
+            }
+        });
+        $.ajax({
+            url: siteRoot + '/constant/industries',
+            type: 'get',
+            success: function (data, textStatus, jqxhr) {
+                $('#company-industry').find('select').first().append(create_options(data));
+            }
+        });
+        $.ajax({
+            url: siteRoot + '/constant/sizes',
+            type: 'get',
+            success: function (data, textStatus, jqxhr) {
+                $('#company-size').find('select').first().append(create_options(data));
+            }
+        });
+        $.ajax({
+            url: siteRoot + '/constant/languages',
+            type: 'get',
+            success: function (data, textStatus, jqxhr) {
+                $('#company-language').find('select').first().append(create_options(data));
+            }
+        });
+        var selects = $('#company-edit-form').find('select');
+        var inputs = $('#company-edit-form').find('input');
+        inputs.removeAttr('disabled');
+        inputs.removeClass('no_field');
+        selects.removeAttr('disabled');
+        selects.removeClass('no_field');
+        $(this).attr('hidden', 'hidden');
+        $('#company-edit-form').find('input[type=submit]').removeAttr('hidden');
+
+    });
+
+    $('#company-edit-form').on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: siteRoot + $(this).attr('action'),
+            type: 'put',
+            data: $(this).serializeArray(),
+            success: function (data, textStatus, jqxhr) {
+                document.location.reload();
+            },
+            error: function (jqxhr, textStatus, errorThrown) {
+                // todo: display errors
+                var errors = jqxhr.responseJSON;
+                msg = "Errors:\n";
+                for (var key in errors) {
+                    msg += key + ": " + errors[key][0] + '\n';
+                }
+                alert(msg);
+            }
+        });
+    });
 
 });
